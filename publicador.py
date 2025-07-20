182bde0c78686560cd2e2bb343ab925da5caf1fe
Gugu120901 <gugualex202@gmail.com>
2025-07-20 15:37:42 +0100

    Delete publicador.py

--- publicador.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def enviar_mensagem(token, channel_id, text):
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    payload = {"chat_id": channel_id, "text": text, "parse_mode": "HTML"}
-    resp = requests.post(url, data=payload)
-    resp.raise_for_status()
